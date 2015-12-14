b8697605c206c9a434e3b2ef4702510bb0b56eaf
zhihao wang <hao@opentown.cn>
2015-12-14 17:38:06 +0800

    format code

--- Parser.scala
+++ Parser.scala
@@ -1,43 +1,43 @@
 import scala.collection.immutable.{ Map, Set }
-import scala.util.parsing.combinator.{ JavaTokenParsers, Parsers}
+import scala.util.parsing.combinator.{ JavaTokenParsers, Parsers }
 import scala.util.parsing.input._
 import scala.language.implicitConversions
 
 object SearchOperatorsParser extends JavaTokenParsers {
 	type Operators = Map[String, Set[String]]
-	
+
 	def Operators() = Map[String, Set[String]]()
 
 	def Operators(a: String, b: Set[String]) = {
 		Map[String, Set[String]](a -> b)
-	}	
-	def merge(a : Operators, b : Operators) : Operators = {
-		a ++ b.map{case (k, v) => k -> (a.getOrElse(k, Set()) ++ v)}	
+	}
+	def merge(a: Operators, b: Operators): Operators = {
+		a ++ b.map { case (k, v) => k -> (a.getOrElse(k, Set()) ++ v) }
 	}
 
-	def values : Parser[Set[String]] = ident ~ rep( "," ~ ident ) ^^ {
+	def values: Parser[Set[String]] = ident ~ rep("," ~ ident) ^^ {
 		//Question: why `Set(value) /: list` would fail?
 		case value ~ list => list.foldLeft(Set(value)) {
 			case (values, "," ~ value) => values + value
-		} 		
+		}
 	}
-	
-	def kvPair : Parser[Operators] = ident ~ ":" ~ values ^^ {
-		case key ~ ":" ~ values => 
+
+	def kvPair: Parser[Operators] = ident ~ ":" ~ values ^^ {
+		case key ~ ":" ~ values =>
 			Operators(key, values)
-		
+
 	}
 
 	//Question: why `ident | kvPair` would fail ?
-	def operators : Parser[Operators] = rep(  kvPair | ident ) ^^ {
+	def operators: Parser[Operators] = rep(kvPair | ident) ^^ {
 		case list => list.foldLeft(Operators()) {
 			case (operators, plaintext: String) => merge(operators, Operators("plaintext", Set(plaintext)))
 			case (operators, kvPair: Operators) => merge(operators, kvPair)
-		} 
+		}
 	}
 
-     def apply(input: String): Operators = parseAll(operators, input) match {
-       case Success(result, _) => result
-       case failure : NoSuccess => scala.sys.error(failure.msg)
-     }
+	def apply(input: String): Operators = parseAll(operators, input) match {
+		case Success(result, _) => result
+		case failure: NoSuccess => scala.sys.error(failure.msg)
+	}
 }
